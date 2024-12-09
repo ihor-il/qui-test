7558871c20dd988818c61f4939073b772be13339
Ihor Lupyna <62049009+ihor-il@users.noreply.github.com>
2024-12-09 15:22:03 +0200

    added base components

--- WeatherApp.Web/src/app/app.component.ts
+++ WeatherApp.Web/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
+import { NgTemplateOutlet } from '@angular/common';
+import { SearchComponent } from './components/search/search.component';
+import { HistoryComponent } from "./components/history/history.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatTabsModule, SearchComponent, HistoryComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'weather-app-web';
