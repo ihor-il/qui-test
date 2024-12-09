45b9b9dd959e8d85403d9e1ed96b4754538f14ef
Ihor Lupyna <62049009+ihor-il@users.noreply.github.com>
2024-12-09 15:58:28 +0200

    ran out of time I guess lol

--- WeatherApp.Web/src/app/components/history/history.component.ts
+++ WeatherApp.Web/src/app/components/history/history.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherForecastService } from '../../services/weather-forecast.service';
+import { Observable, Subscription } from 'rxjs';
+import { WeatherForecastModel } from '../../model/weather-forecast.model';
+import { AsyncPipe } from '@angular/common';
+import { WeatherDataComponent } from '../weather-data/weather-data.component';
 
 @Component({
   selector: 'app-history',
-  imports: [],
+  imports: [AsyncPipe, WeatherDataComponent],
   templateUrl: './history.component.html',
   styleUrl: './history.component.scss',
   standalone: true,
 })
-export class HistoryComponent {}
+export class HistoryComponent implements OnInit {
+  //FIXME: Session cookie is not saving automatically
+
+  public data$?: Observable<WeatherForecastModel[]>;
+  private sub?: Subscription;
+
+  get loading(): boolean {
+    return this.sub != undefined && !this.sub?.closed;
+  }
+
+  constructor(private service: WeatherForecastService) { }
+
+  ngOnInit(): void {
+    this.reload();
+  }
+
+  reload() {
+    this.data$ = this.service.getHistory();
+  }
+}
--- WeatherApp.Web/src/app/components/search/search.component.ts
+++ WeatherApp.Web/src/app/components/search/search.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatFormField, MatInput } from '@angular/material/input';
+import { WeatherForecastService } from '../../services/weather-forecast.service';
+import {
+  Observable,
+  Subject,
+  Subscribable,
+  Subscription,
+  takeUntil,
+} from 'rxjs';
+import { WeatherForecastModel } from '../../model/weather-forecast.model';
+import { WeatherDataComponent } from '../weather-data/weather-data.component';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [
+    MatFormField,
+    MatInput,
+    MatButton,
+    FormsModule,
+    WeatherDataComponent,
+    NgIf,
+    AsyncPipe,
+  ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss',
   standalone: true,
 })
-export class SearchComponent {}
+export class SearchComponent implements OnDestroy {
+  private readonly _unsub$ = new Subject<void>();
+
+  public result?: WeatherForecastModel;
+  public sub?: Subscription;
+
+  public prompt: string = '';
+
+  constructor(private service: WeatherForecastService) {}
+
+  ngOnDestroy(): void {
+    this._unsub$.next();
+    this._unsub$.complete();
+  }
+
+  search() {
+    this.sub = this.service
+      .search(this.prompt)
+      .pipe(takeUntil(this._unsub$))
+      .subscribe((d) => (this.result = d));
+  }
+}
